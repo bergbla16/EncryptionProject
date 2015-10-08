db7775145cd62cdff774cad9e7f9c2bc69253a5b
NathanVaska <legionofvaska@gmail.com>
2015-10-08 13:06:07 -0700

    Basic work on abstract Text Decryption Class
    As in title

--- TextDecryption.cs
+++ TextDecryption.cs
@@ -5,9 +5,24 @@ namespace EncryptionProject
 
     public abstract class TextDecryption
     {
-        public TextDecryption()
+        public TextDecryption(string algorithmName)
         {
-            //this is a test
+            algorithmName = this.algorithmName;
+
+        }
+
+        public string algorithmName;
+
+        public abstract string getInfo(); //Returns description of algorithm
+        public abstract string decryptWithForce(string encryptedMessage); //Decrypts method using force
+    }
+
+    public class DecryptCaesarCypher : TextDecryption
+    {
+           public DecryptCaesarCypher(string algorithmName)
+        {
+            this.algorithmName = algorithmName;
         }
     }
+
 }
