3cf0a8dc46102c89031f0190258e4dd58155bdc0
NathanVaska <legionofvaska@gmail.com>
2015-10-21 14:44:55 -0700

    Work
    Work

--- EncryptionProject/ForcedDecryption/TextDecryption.cs
+++ EncryptionProject/ForcedDecryption/TextDecryption.cs
@@ -4,11 +4,13 @@ namespace EncryptionProject
 {
 
     public abstract class TextDecryption
-    { 
-        public TextDecryption(string algorithmName)
+    {
+
         public const int LOWER_ASCII_BOUND = 97;
         public const int UPPER_ASCII_BOUND = 122;
-        public const int[] ASCII_EXCEPTIONS = new int[1] {32}; 
+        public const int[] ASCII_EXCEPTIONS = new int[1] {32};
+
+        public TextDecryption(string algorithmName)
         {
             algorithmName = this.algorithmName;
 
@@ -35,31 +37,38 @@ namespace EncryptionProject
         //Decrypt specified cypher with force
         public string decryptWithForce(string encryptedMessage)
         {
-
+            bool isException = false;
 
             byte[] originalMessageInAscii = Encoding.ASCII.GetBytes(encryptedMessage);
             byte[] changedMessage = new Array(originalMessageInAscii.GetLength());
-
-            int length = originalMessageInAscii.GetLength();
 
             //Checks each letter of the alphabet (lower case only)
             for(int i = 0; i < 26; i++)
             {
                 //Shifts ascii value in originalMessage by i to check
-                for (int j = 0; j < length; j++)
+                for (int j = 0; j < originalMessageInAscii.GetLength(); j++)
                 {
-                    changedMessage[j] = originalMessageInAscii[j] + i;
+                    //changedMessage[j] = originalMessageInAscii[j] + i;
+                    
+                    for()
 
-                    if
+
+                    if (originalMessageInAscii[j] + i > UPPER_ASCII_BOUND)
+                    {
+                        changedMessage[j] = LOWER_ASCII_BOUND + (originalMessageInAscii[j] + i - UPPER_ASCII_BOUND);
+                    } else if (originalMessageInAscii + 1 == ASCII_EXCEPTIONS[k])
+                    {
+                        for (int k = 0; k < ASCII_EXCEPTIONS.GetLength(); k++)
+                        {
+                            changedMessage[j] = ASCII_EXCEPTIONS[k];
+                        }
+                    }
+
+
+
                     Console.WriteLine(Encoding.ASCII.GetString(changedMessage, 0, length));
                 }
             }
-            
-            /*
-            foreach (byte b in messageInAscii)
-            {
-                Console.WriteLine(b);
-            }*/
         }
 
     }
