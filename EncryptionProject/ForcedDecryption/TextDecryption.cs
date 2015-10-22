2283f4949ca4a6f5b0c9726876b57f10e2ab0165
NathanVaska <legionofvaska@gmail.com>
2015-10-22 13:12:33 -0700

    Changes

--- EncryptionProject/ForcedDecryption/TextDecryption.cs
+++ EncryptionProject/ForcedDecryption/TextDecryption.cs
@@ -10,6 +10,10 @@ namespace EncryptionProject
         public const int UPPER_ASCII_BOUND = 122;
         public const int[] ASCII_EXCEPTIONS = new int[1] {32};
 
+        //IE
+        //Y:121 + 3 = 124 - 122 = 2 + 97 = 99 - 1 = 98 = b
+        // General Formula: Ascii value + shift - upperbound +lowerbound - 1 = Desired Ascii value  
+
         public TextDecryption(string algorithmName)
         {
             algorithmName = this.algorithmName;
@@ -37,25 +41,42 @@ namespace EncryptionProject
         //Decrypt specified cypher with force
         public string decryptWithForce(string encryptedMessage)
         {
-            bool isException = false;
+            bool hasException = false;
+            List<int> exceptionLocation = new List<int>();
 
             byte[] originalMessageInAscii = Encoding.ASCII.GetBytes(encryptedMessage);
             byte[] changedMessage = new Array(originalMessageInAscii.GetLength());
 
+            //Checks if there are any exception in ACSII, finds location if there are and adds them to list exceptionLocation
+            for (int i = 0; i < originalMessageInAscii.GetLength(); i++)
+            {
+                for (int j = 0; j < ASCII_EXCEPTIONS.GetLength(); j++)
+                    if (originalMessageInAscii[i] == ASCII_EXCEPTIONS[j])
+                    {
+                        hasException = true;
+                        exceptionLocation.add(i);
+                    }
+
+            }
             //Checks each letter of the alphabet (lower case only)
-            for(int i = 0; i < 26; i++)
+            for (int i = 1; i < 26; i++)
             {
                 //Shifts ascii value in originalMessage by i to check
                 for (int j = 0; j < originalMessageInAscii.GetLength(); j++)
                 {
-                    //changedMessage[j] = originalMessageInAscii[j] + i;
-                    
-                    for()
-
-
-                    if (originalMessageInAscii[j] + i > UPPER_ASCII_BOUND)
+                   
+                    // if there is an exception, do not alter that location
+                    if (hasException)
                     {
-                        changedMessage[j] = LOWER_ASCII_BOUND + (originalMessageInAscii[j] + i - UPPER_ASCII_BOUND);
+                        for(int n = 0; n < exceptionLocation.GetLength(); n++)
+                        {
+                            if (exceptionLocation[n] == j)
+                            {
+                                changedMessage = originalMessageInAscii[j];
+                            }
+                        }
+                    } else if (originalMessageInAscii[j] + i > UPPER_ASCII_BOUND){
+                        changedMessage[j] = LOWER_ASCII_BOUND + (originalMessageInAscii[j] + i - UPPER_ASCII_BOUND - 1);
                     } else if (originalMessageInAscii + 1 == ASCII_EXCEPTIONS[k])
                     {
                         for (int k = 0; k < ASCII_EXCEPTIONS.GetLength(); k++)
